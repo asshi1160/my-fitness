6df55ae75f5a04e583bb71240a2b94ad498b77e3
asshi1160 <104013436+asshi1160@users.noreply.github.com>
2023-06-04 12:49:14 +0900

    create Training model

---
+++ app/models/Training.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+)
+
+type Training struct {
+	ID            uint     `gorm:"primaryKey"`
+	BodyPart      string   `gorm:"type:varchar(10);not null"`
+	TrainingType  string   `gorm:"type:varchar(20);not null"`
+	TrainingName  string   `gorm:"type:varchar(30);unique;not null"`
+	TrainingForms []string `gorm:"type:varchar(50);not null;serializer:json"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
