8a0eceef4798c6d48e13a41e66c7f101fe968983
asshi1160 <104013436+asshi1160@users.noreply.github.com>
2023-06-04 13:43:45 +0900

    add migration to main

--- app/main.go
+++ app/main.go
@@ -4,9 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"my-fitness/database/connection"
+	"my-fitness/models"
 )
 
 func main() {
+	connection.Open()
+	defer connection.Close()
+
+	db := connection.GetConn()
+	err := db.AutoMigrate(
+		&models.User{},
+		&models.Weight{},
+		&models.Training{},
+		&models.TrainingLog{},
+	)
+	if err != nil {
+		panic("migration error")
+	}
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
